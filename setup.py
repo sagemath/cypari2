2a6549f9749b19aa4adb4928241885aa725a3f42
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2017-01-13 14:50:22 +0100

    two fixes in setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,23 @@ import autogen
 import os, sys
 from glob import glob
 
+print("path", sys.path)
 opj = os.path.join
 
 cythonize_dir = "build"
 
-kwds = dict(include_dirs=["cypari2"],# cythonize_dir, opj(cythonize_dir, "cypari2")],
+# TODO
+# We need CYSIGNALS_DIR because cython generates a C file
+# with the line
+#     # include "struct_signals.h"
+# which needs to be found by gcc... would be better to have
+#     # include "cysignals/struct_signals.h"
+# It might be a bug in cysignals (talk to Jeroen). I am not sure
+# this is a good way to achieve this
+import cysignals
+CYSIGNALS_DIR=cysignals.__path__
+
+kwds = dict(include_dirs=["cypari2"] + [CYSIGNALS_DIR],# cythonize_dir, opj(cythonize_dir, "cypari2")],
             libraries=["pari"],
             depends=glob(opj("cypari2", "*.h")))
 
@@ -25,10 +37,15 @@ extensions = [
     Extension("cypari2.stack", ["cypari2/stack.pyx"], **kwds),
 ]
 
+# TODO
+# This needs to be run *before* cythonize!
+# (see also the commented line in build_ext.run below)
+autogen.rebuild()
+
 class build_ext(_build_ext):
     def run(self):
         # Generate auto_gen.pxi and auto_instance.pxi
-        autogen.rebuild()
+        #autogen.rebuild()
         return _build_ext.run(self)
 
 setup(
