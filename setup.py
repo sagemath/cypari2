d7f6b9ac269c7a89ce74279fb2fb8d62bd8ed2dc
Jeroen Demeyer <jdemeyer@cage.ugent.be>
2017-01-30 10:09:22 +0100

    Simplify setup.py a lot

--- setup.py
+++ setup.py
@@ -1,51 +1,23 @@
 from setuptools import setup
 from distutils.command.build_ext import build_ext as _build_ext
-from distutils.sysconfig import get_python_inc
 from setuptools.extension import Extension
-from Cython.Build.Dependencies import cythonize
-import autogen
 
-import os, sys
-from glob import glob
+import os
+import sys
 
-opj = os.path.join
 
-cythonize_dir = "build"
-
-# TODO
-# We need CYSIGNALS_DIR because cython generates a C file
-# with the line
-#     # include "struct_signals.h"
-# which needs to be found by gcc... would be better to have
-#     # include "cysignals/struct_signals.h"
-# It might be a bug in cysignals (talk to Jeroen). I am not sure
-# this is a good way to achieve this
-import cysignals
-CYSIGNALS_DIR=cysignals.__path__
-
-kwds = dict(include_dirs=["cypari2"] + CYSIGNALS_DIR,# cythonize_dir, opj(cythonize_dir, "cypari2")],
-            libraries=["pari"],
-            depends=glob(opj("cypari2", "*.h")))
-
-extensions = [
-    Extension("cypari2.closure", ["cypari2/closure.pyx"], **kwds),
-    Extension("cypari2.convert", ["cypari2/convert.pyx"], **kwds),
-    Extension("cypari2.gen", ["cypari2/gen.pyx"], **kwds),
-    Extension("cypari2.handle_error", ["cypari2/handle_error.pyx"], **kwds),
-    Extension("cypari2.pari_instance", ["cypari2/pari_instance.pyx"], **kwds),
-    Extension("cypari2.stack", ["cypari2/stack.pyx"], **kwds),
-]
-
-# TODO
-# This needs to be run *before* cythonize!
-# (see also the commented line in build_ext.run below)
-autogen.rebuild()
-
+# Adapted from Cython's new_build_ext
 class build_ext(_build_ext):
-    def run(self):
-        # Generate auto_gen.pxi and auto_instance.pxi
-        #autogen.rebuild()
-        return _build_ext.run(self)
+    def finalize_options(self):
+        # Generate auto-generated sources from pari.desc
+        from autogen import rebuild
+        rebuild()
+
+        from Cython.Build.Dependencies import cythonize
+        self.distribution.ext_modules[:] = cythonize(
+            self.distribution.ext_modules, include_path=sys.path)
+        _build_ext.finalize_options(self)
+
 
 setup(
     name='cypari2',
@@ -55,8 +27,7 @@ setup(
     author='Many people',
     author_email="sage-devel@googlegroups.com",
     license='GNU General Public License, version 2 or later',
-    # Install cycsignals before this
-    ext_modules=cythonize(extensions, include_path=sys.path),
+    ext_modules=[Extension("*", ["cypari2/*.pyx"])],
     keywords='PARI/GP number theory',
     packages=['cypari2'],
     package_dir={'cypari2': 'cypari2'},
