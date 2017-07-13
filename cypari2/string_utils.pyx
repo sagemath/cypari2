d9efdf88a3ff8555fd10f59394b12076dfc6a6ee
Vincent Delecroix <vincent.delecroix@u-bordeaux.fr>
2017-07-13 13:46:20 +0200

    compile time PY_MAJOR_VERSION + inlined to_string

--- cypari2/string_utils.pyx
+++ cypari2/string_utils.pyx
@@ -5,7 +5,6 @@ Conversion functions for bytes/unicode
 
 import sys
 encoding = sys.getfilesystemencoding()
-cdef int PY_MAJOR_VERSION = sys.version_info.major
 
 cpdef bytes to_bytes(s):
     """
@@ -40,7 +39,7 @@ cpdef unicode to_unicode(s):
     >>> s2 = to_unicode('hello')
     >>> s3 = to_unicode(u'hello')
     >>> import sys
-    >>> u_type = (unicode if sys.version_info.major == 2 else str)
+    >>> u_type = (unicode if sys.version_info.major <= 2 else str)
     >>> type(s1) == type(s2) == type(s3) == u_type
     True
     >>> s1 == s2 == s3 == u'hello'
@@ -53,24 +52,3 @@ cpdef unicode to_unicode(s):
     else:
         raise TypeError
 
-cpdef to_string(s):
-    r"""
-    Converts a bytes and unicode ``s`` to a string.
-
-    String means bytes in Python2 and unicode in Python3
-
-    Examples:
-
-    >>> from cypari2.string_utils import to_string
-    >>> s1 = to_string(b'hello')
-    >>> s2 = to_string('hello')
-    >>> s3 = to_string(u'hello')
-    >>> type(s1) == type(s2) == type(s3) == str
-    True
-    >>> s1 == s2 == s3 == 'hello'
-    True
-    """
-    if PY_MAJOR_VERSION <= 2:
-        return to_bytes(s)
-    else:
-        return to_unicode(s)
