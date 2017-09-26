e3f02be619ff81aafc76b4919b0634bf6b20e381
Vincent Klein <vincent.klein@u-bordeaux.fr>
2017-09-26 10:23:15 +0200

    functions utils for test units

---
+++ tests/unittests/testutils.py
@@ -0,0 +1,7 @@
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * int(n/2)
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i/2]:
+            sieve[i*i/2::i] = [False] * ((n-i*i-1)/(2*i)+1)
+    return [2] + [2*i+1 for i in xrange(1,n/2) if sieve[i]]
