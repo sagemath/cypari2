677b3d3b95db85e4a348c57afdf33778d8328817
Vincent Klein <vincent.klein@u-bordeaux.fr>
2017-11-10 15:34:14 +0100

    alltest.py run a test unit only if his required extension are installed.

--- tests/unittests/alltest.py
+++ tests/unittests/alltest.py
@@ -1,5 +1,8 @@
 import unittest
 import sys
+from cypari2 import Pari, PariError
+
+pari = Pari()
 
 testmodules = [
     'bern',
@@ -59,18 +62,36 @@ require_seadata = ["ellmodulareqn"]
 galpol_installed = True
 seadata_installed = True
 
+# test extensions presence.
+try:
+    pari.ellmodulareqn(2)
+except PariError as e:
+    if "error opening seadata file" in str(e):
+        seadata_installed = False
+    else:
+        raise e
+
+try:
+    pari.galoisgetpol(8)
+except PariError as e:
+    if "error opening galpol file" in str(e):
+        galpol_installed = True
+    else:
+        raise e
+
 suite = unittest.TestSuite()
 
 
 for t in testmodules:
-    try:
-        # If the module defines a suite() function, call it to get the suite.
-        mod = __import__(t, globals(), locals(), ['suite'])
-        suitefn = getattr(mod, 'suite')
-        suite.addTest(suitefn())
-    except (ImportError, AttributeError):
-        # else, just load all the test cases from the module.
-        suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+    if not ((t in require_galpol and not galpol_installed) or (t in require_seadata and not seadata_installed)):
+        try:
+            # If the module defines a suite() function, call it to get the suite.
+            mod = __import__(t, globals(), locals(), ['suite'])
+            suitefn = getattr(mod, 'suite')
+            suite.addTest(suitefn())
+        except (ImportError, AttributeError):
+            # else, just load all the test cases from the module.
+            suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
 
 res = unittest.TextTestRunner().run(suite)
 retcode = 0 if res.wasSuccessful() else 1
