457e40a490555002f48085354e10862cc90170ad
Tobias Diez <code@tobiasdiez.de>
2025-08-31 10:01:37 +0800

    Generate files into the build dir

--- autogen/__init__.py
+++ autogen/__init__.py
@@ -1,19 +1,25 @@
 from __future__ import absolute_import
 
 import glob
-import os
-from os.path import join, getmtime, exists
+from os.path import exists, getmtime, join
+from pathlib import Path
 
 from .generator import PariFunctionGenerator
 from .paths import pari_share
 
 
-def rebuild(force=False):
-    pari_module_path = 'cypari2'
-    src_files = [join(pari_share(), 'pari.desc')] + \
-                 glob.glob(join('autogen', '*.py'))
-    gen_files = [join(pari_module_path, 'auto_paridecl.pxd'),
-                 join(pari_module_path, 'auto_gen.pxi')]
+def rebuild(force: bool = False, output: None | str = None):
+    if output is None:
+        output = "cypari2"
+    output_dir = Path(output)
+    # Ensure output directory exists
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    src_files = [join(pari_share(), "pari.desc")] + glob.glob(join("autogen", "*.py"))
+    gen_files = [
+        output_dir / "auto_paridecl.pxd",
+        output_dir / "auto_gen.pxi",
+    ]
 
     if not force and all(exists(f) for f in gen_files):
         src_mtime = max(getmtime(f) for f in src_files)
@@ -22,5 +28,5 @@ def rebuild(force=False):
         if gen_mtime > src_mtime:
             return
 
-    G = PariFunctionGenerator()
+    G = PariFunctionGenerator(output_dir)
     G()
