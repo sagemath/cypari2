271efe593d2bf06a7a04fc76bb707c0c12d3839f
Tobias Diez <code@tobiasdiez.de>
2025-10-01 20:14:43 +0800

    make autogen tests optional (not in wheel)

--- tests/rundoctest.py
+++ tests/rundoctest.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import traceback
 
 # Autogen tests must be run in the root dir, and with the proper module path
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
@@ -9,7 +10,6 @@ os.chdir(path)
 sys.path.append(path)
 import doctest
 
-import autogen
 import cypari2
 
 # The doctests assume utf-8 encoding
@@ -25,19 +25,29 @@ cypari2.handle_error.PariError.__module__ = "__main__"
 pari = cypari2.Pari()
 pari.default("debugmem", 0)
 
+modules = [cypari2.closure, cypari2.convert, cypari2.gen,
+            cypari2.handle_error, cypari2.pari_instance, cypari2.stack,
+            cypari2.string_utils]
+try:
+      import autogen
+      modules.extend([
+            autogen.doc, autogen.generator, autogen.parser])
+except ImportError:
+        print("Skipping autogen tests")
 
 failed = 0
 attempted = 0
-for mod in [cypari2.closure, cypari2.convert, cypari2.gen,
-            cypari2.handle_error, cypari2.pari_instance, cypari2.stack,
-            cypari2.string_utils,
-            autogen.doc, autogen.generator, autogen.parser]:
-
+for mod in modules:
     print("="*80)
     print("Testing {}".format(mod.__name__))
-    test = doctest.testmod(mod, optionflags=doctest.ELLIPSIS|doctest.REPORT_NDIFF, verbose=False)
-    failed += test.failed
-    attempted += test.attempted
+    try:
+        test = doctest.testmod(mod, optionflags=doctest.ELLIPSIS|doctest.REPORT_NDIFF, verbose=False)
+        failed += test.failed
+        attempted += test.attempted
+    except Exception:
+        traceback.print_exc()
+        failed += 1
+        attempted += 1
 
 print("="*80)
 print("Summary result for cypari2:")
